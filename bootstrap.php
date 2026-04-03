f616b3134c75da7b323831105b7b5a2b89c25128
Adam Greenough <adam@greenough.me>
2026-04-03 15:01:24 +0000

    Bump Ava version to 26.4.2

--- bootstrap.php
+++ bootstrap.php
@@ -16,7 +16,7 @@ if (php_sapi_name() !== 'cli' && !defined('AVA_START')) {
 }
 
 // Ava version (CalVer: YY.M.PATCH - e.g., 26.2.0 = first patch of Feb 2026)
-define('AVA_VERSION', '26.4.1');
+define('AVA_VERSION', '26.4.2');
 
 // Ensure we have a root constant
 if (!defined('AVA_ROOT')) {
